d11b684e8a0b6ce85a5d2fa9e97e0a00f8ffcfa1
Hodgey <Hodgey@Hodgey-HP>
2014-09-08 19:12:43 -0500

    calculator service and shapes

--- src/java/controller/RectangleController.java
+++ src/java/controller/RectangleController.java
@@ -1,46 +1,84 @@
-package controller;
-
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import model.RectangleModel;
 
-
- 
+@WebServlet(urlPatterns = {"/calculate"})
 public class RectangleController extends HttpServlet {
+private static final String RESULT_PAGE = "result.jsp";
+private static final String CALC_TYPE = "calcType";
 
-protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-throws ServletException, IOException {
-response.setContentType("text/html;charset=UTF-8");
-String length = request.getParameter("length");
-String width = request.getParameter("width");
-RectangleModel calculator = new RectangleModel();
-String area = calculator.calculateArea(length, width);
-request.setAttribute("area", area);
-RequestDispatcher view = request.getRequestDispatcher("/rectangleResponse.jsp");
-view.forward(request, response);
-}
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+                String calcType = request.getParameter(CALC_TYPE);
+                CalculatorService cs = new CalculatorService();
+                if(calcType.equalsIgnoreCase("Rectangle")){
+                   cs.getResultFromType(request, Shapes.RECTANGLE);
+                } else if(calcType.equalsIgnoreCase("Circle")){
+                    cs.getResultFromType(request, Shapes.CIRCLE);
+                } else if(calcType.equalsIgnoreCase("Triangle")){
+                    cs.getResultFromType(request, Shapes.TRIANGLE);
+                }
+            
 
-@Override
-protected void doGet(HttpServletRequest request, HttpServletResponse response)
-throws ServletException, IOException {
-processRequest(request, response);
-}
+            RequestDispatcher view =
+                request.getRequestDispatcher(RESULT_PAGE);
+        view.forward(request, response);
+        }
+    }
 
-@Override
-protected void doPost(HttpServletRequest request, HttpServletResponse response)
-throws ServletException, IOException {
-processRequest(request, response);
-}
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
 
-@Override
-public String getServletInfo() {
-return "Desc";
-}
-}
-    
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
 
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
